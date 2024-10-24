923bd2542db2b38fceecb13135d2677c6dca28b0
code-walker-23 <chhabravinay549@gmail.com>
2024-10-24 11:15:00 +0530

    building-react-email-services

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,6 +4,9 @@ import SectionHeading from "./SectionHeading";
 import { useSectionInView } from "@/hooks/useSectionInView";
 import { FaPaperPlane } from "react-icons/fa";
 import { motion } from "framer-motion";
+
+const handleSubmit = () => {};
+
 const Contact = () => {
   const { ref } = useSectionInView({
     sectionName: "Contact",
@@ -28,8 +31,8 @@ const Contact = () => {
         once: true,
       }}
     >
-      <SectionHeading>Contact</SectionHeading>
-      <p className="text-gray-700 -mt-6">
+      <SectionHeading>Contact Me</SectionHeading>
+      <p className="text-gray-700 -mt-5">
         Please contact me directly at{" "}
         <a
           className="underline text-gray-600 hover:text-gray-800 transition duration-200 font-semibold"
@@ -39,12 +42,26 @@ const Contact = () => {
         </a>{" "}
         or through this form
       </p>
-      <form className="mt-10 flex flex-col">
+      <form
+        className="mt-10 flex flex-col"
+        onSubmit={handleSubmit}
+        action={(formData) => {
+          console.log(formData);
+        }}
+      >
         <input
           type="email"
-          className="h-14 rounded-lg border border-black/10"
+          className="px-4 h-14 rounded-lg border border-black/10"
+          required
+          maxLength={500}
+          placeholder="Your Email"
         />
-        <textarea className="h-52 my-3 rounded-lg border border-black/10 " />
+        <textarea
+          className="h-52 my-3 rounded-lg border border-black/10 p-4"
+          placeholder="Your message"
+          required
+          maxLength={500}
+        />
         <button
           type="submit"
           className="group h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all flex items-center justify-center gap-2 focus:scale-110 hover:scale-105 active:scale-105 hover:bg-gray-950"
