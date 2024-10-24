0e296f5d49d2d69b65ac6bd5ad7ce6bc0d5c2a96
code-walker-23 <chhabravinay549@gmail.com>
2024-10-24 22:56:49 +0530

    style-completed

--- components/ContactFormEmail.tsx
+++ components/ContactFormEmail.tsx
@@ -22,10 +22,10 @@ const ContactFormEmail = ({ message, senderEmail }: ContactFormEmailProps) => {
       <Head />
       <Preview>New message from your portfolio</Preview>
       <Tailwind>
-        <Body>
+        <Body className="bg-gray-100 text-black">
           <Container>
-            <Section>
-              <Heading>
+            <Section className="bg-white border border-black/10 my-10 px-10 py-4 rounded-md">
+              <Heading className="leading-tight">
                 You received the following message from the contact form
               </Heading>
               <Text>{message}</Text>
