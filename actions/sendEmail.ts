5b5e08f52557c0b840099258b81ef4d463e61d3a
code-walker-23 <chhabravinay549@gmail.com>
2024-10-24 18:51:54 +0530

    building-email-service-uisng-resend

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,6 +1,16 @@
 "use server";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export const sendEmail = async (formData: FormData) => {
   console.log("Running on server");
   console.log(formData.get("senderEmail"));
   console.log(formData.get("message"));
+  resend.emails.send({
+    from: "vinaychhabra786@gmail.com",
+    to: "chhabravinay549@gmail.com",
+    subject: "Message from contact from",
+    text: "Hello World!",
+  });
 };
