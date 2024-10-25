d4dfe1be24c550cb37495db147e0976fdaa28019
code-walker-23 <chhabravinay549@gmail.com>
2024-10-25 11:30:33 +0530

    building

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { BsSun } from "react-icons/bs";
 
 const ThemeSwitch = () => {
-  return <div>ThemeSwitch</div>;
+  return (
+    <button className="fixed bottom-5 right-5">
+      <BsSun />{""}
+    </button>
+  );
 };
 
 export default ThemeSwitch;
