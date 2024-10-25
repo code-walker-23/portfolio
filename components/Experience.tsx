b20ec914457bd9716c35838a5f77505b99d02013
code-walker-23 <chhabravinay549@gmail.com>
2024-10-25 18:29:54 +0530

    styling-dark-mode

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -25,9 +25,12 @@ const Experience = () => {
             key={index}
             contentStyle={{
               background:
-                theme === "light" ? "#f3f4f6" : "rgba(255,255,255,0.05)",
-              boxShadow: "0 4px 10px rgba(0,0,0,0.1)",
-              border: "1px solid #e5e7eb",
+                theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
+              boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
+              border:
+                theme === "light"
+                  ? "1px solid #e5e7eb"
+                  : "1px solid rgba(255, 255, 255, 0.1)",
               textAlign: "left",
               padding: "1.5rem 2rem",
               borderRadius: "8px",
@@ -36,27 +39,38 @@ const Experience = () => {
               borderRight:
                 theme === "light"
                   ? "0.4rem solid #9ca3af"
-                  : "0.4rem solid rgba(255,255,255,0.5)",
+                  : "0.4rem solid rgba(255, 255, 255, 0.5)",
             }}
             date={experience.date}
             icon={experience.icon}
             iconStyle={{
               background:
-                theme === "light" ? "#ffffff" : "rgba(255,255,255,0.5)",
-
+                theme === "light" ? "#ffffff" : "rgba(255, 255, 255, 0.5)",
               fontSize: "1.5rem",
               padding: "0.5rem",
               borderRadius: "50%",
-              boxShadow: "0 2px 5px rgba(0,0,0,0.1)",
+              boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)",
             }}
           >
-            <h3 className="font-semibold text-lg text-gray-800">
+            <h3
+              className={`font-semibold text-lg ${
+                theme === "light" ? "text-gray-800" : "text-white"
+              }`}
+            >
               {experience.title}
             </h3>
-            <p className="font-medium text-md text-gray-600 italic">
+            <p
+              className={`font-medium text-md ${
+                theme === "light" ? "text-gray-600" : "text-gray-300"
+              } italic`}
+            >
               {experience.company}
             </p>
-            <p className="!mt-1 font-normal text-gray-500 dark:text-white/75">
+            <p
+              className={`!mt-1 font-normal ${
+                theme === "light" ? "text-gray-500" : "text-white/75"
+              }`}
+            >
               {experience.description}
             </p>
           </VerticalTimelineElement>
