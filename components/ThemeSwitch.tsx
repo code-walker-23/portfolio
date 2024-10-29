e6446a3c5d84e660b51563ec79abfa68f9b53197
Vinay Chhabra <116024992+code-walker-23@users.noreply.github.com>
2024-10-29 14:20:04 +0530

    Update ThemeSwitch.tsx

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -32,7 +32,7 @@ const ThemeSwitch = () => {
   console.log(theme);
   return (
     <button
-      className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 outline-none shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
+      className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 outline-none shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950 dark:border-none"
       onClick={toggleTheme}
     >
       {!(theme === "light") ? <BsMoon /> : <BsSun />}
